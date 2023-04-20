e213c755a62a66c47029c6ae79663c4aa45a02d6
Nico <jnch2005@gmail.com>
2023-04-20 18:24:07 -0500

    update final

--- ciclos_4.py
+++ ciclos_4.py
@@ -1,7 +1,11 @@
-y = lambda mensaje: int (input(mensaje))
-x = y ("dar un numero:")
-c = 0
-while x > 0 :
-    c += 1 
-    x = x // 10
-print(c) 
+give_number = lambda mensaje: int (input(mensaje))#lamda para fragmentar el codigo el cual nos permite ingresar un integer
+show_message = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
+def ciclo_counter(numero):#creacion de una funcion con un lopp
+    acumuler = 0#se declara el acumulador
+    while numero > 0 :#condicion
+        acumuler += 1 #el acumulador 
+        numero = numero // 10 #esta es la clave de codigo por cada vez que se repita el codigo se divide en 10 pero entero
+        show_message(acumuler)#se llama al lamda y se muestra el acumuler
+
+number = give_number ("dar un numero:")#se llama al give number
+do_loop = ciclo_counter(number)# se llama a ciclo
