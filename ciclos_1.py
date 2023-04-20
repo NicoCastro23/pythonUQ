9e80e70c3600436f4b372abfeafb2005b29aa9a9
Nico <jnch2005@gmail.com>
2023-04-20 18:12:59 -0500

    final update

--- ciclos_1.py
+++ ciclos_1.py
@@ -1,16 +1,17 @@
 #Este programa pide un numero y da los multipos de 3 desde 6 hasta n donde n > 6
 give_integer = lambda mensaje : int (input(mensaje))#lamda para fragmentar el codigo el cual nos permite ingresar un integer
-show_message = lambda mensaje : f"{mensaje}"#lamda para fragmentar el codigo el cual nos da un show message
+show_message = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
 def ciclo_one(number):  #funcion que pide un number que va desde 6 -n
     for i in range (6,number):
-        condition = condition_loop(i)#se llama la funcion condition tomando  a i que recorre el numero dado por el usuario 
-    return condition
+        if condition_loop(i):#se llama la funcion condition tomando  a i que recorre el numero dado por el usuario 
+            show_message(i)#se llama a show message
 
 def condition_loop (ciclo):#conditional que nos permite dar los multiplos de 3 
     if ciclo % 3 == 0:
-        return show_message(ciclo)#se llama la lamda de show message mostrando el ciclo
-
+        return True
+    else:
+        return False
 give_number = give_integer("ingrese un numero n:")# se llama la lamda que nos permite ingresar un numero
 loop_do = ciclo_one(give_number)#se ejecuta el ciclo
-print(loop_do)#se imprime el cilo
+
     
--- ciclos_3.py
+++ ciclos_3.py
@@ -1,12 +1,14 @@
-number = lambda mensaje : int(input(mensaje))
-show_message = lambda mensaje : f"{mensaje}"
-def ciclo_multiplicacion(numer_one, number_two):
-    z = 0
-    for i in range(0, number_two):
-        z += numer_one # 3+3+3+3
-        return show_message(z)
-give_number = number("numero 1:") # 3
-give_number_two = number("numero 2:") # 4
-ejecute_loop = ciclo_multiplicacion(give_number, give_number_two)
-print(ejecute_loop)
+#Con este codigo se utiliza para multiplicar 2 numeros sin necesidad de *
+number = lambda mensaje : int(input(mensaje))#lamda para fragmentar el codigo el cual nos permite ingresar un integer
+show_message = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
+def ciclo_multiplicacion(numer_one, number_two):#funcion de loop
+    z = 0#se declara z fuera de la variable
+    for i in range(0, number_two):#un loop donde se va desde el cero hasta e numeor 2 
+        z += numer_one # 3+3+3+ #z sirve como acumulador 
+        show_message(z)#se llama a show mwssage
+        
+give_number = number("numero 1:") #se declara numero 1
+give_number_two = number("numero 2:") # se declara numero 2
+ejecute_loop = ciclo_multiplicacion(give_number, give_number_two)#se llama todo e codigo
+
 
