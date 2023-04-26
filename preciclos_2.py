e4639d7a1a8a0c14cf6027b00c301ef6472e7291
Nico <jnch2005@gmail.com>
2023-04-26 11:56:31 -0500

    sirve

--- preciclos_2.py
+++ preciclos_2.py
@@ -1,20 +1,14 @@
+generar_mensaje = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
 
-def main():
-  value = ingrese_real ("ingrese el valor a sumar: ")
-  process = procedimiento_suma (value)
-  mensaje = generar_mensaje (process)
-  mostrar = mostrar_mensaje (mensaje)
-
-main()
-
-generar_mensaje = lambda mensaje : print(mensaje)
-
-
-def ingrese_real (mens):
-    valor = int(input(mens))
-    return valor
+ingrese_real = lambda mensaje : str(input(mensaje))
 
 def  procedimiento_suma (valor):
-  valor = int (valor[0]) + int(valor[1]) + int(valor[2]) + int(valor[3])
+  valor = int (valor[0]) + int (valor[1]) + int (valor[2]) + int (valor[3])
   return valor
-show_message = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
+
+
+value = ingrese_real ("ingrese el valor a sumar: ")
+process = procedimiento_suma (value)
+mensaje = generar_mensaje (process)
+
+
