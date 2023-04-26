50d08f6d592a08bbf5e8da5b1db9a5f38634ddb6
Nico <jnch2005@gmail.com>
2023-04-26 11:18:34 -0500

    Intentandolo hasta morir

--- preciclos_2.py
+++ preciclos_2.py
@@ -1,19 +1,13 @@
+
 def main():
-    value = ingrese_real ("ingrese el valor a sumar: ")
-    process = procedimiento_suma (value)
-    mensaje = generar_mensaje (process)
-    mostrar = mostrar_mensaje (mensaje)
-
-main()
-
-generar_mensaje = lambda mensaje : print(mensaje)
-
-
-def ingrese_real (mens):
-    valor = int(input(mens))
-    return valor
+  value = ingrese_real ("ingrese el valor a sumar: ")
+  process = procedimiento_suma (value)
+  mensaje = generar_mensaje (process)
+  mostrar = mostrar_mensaje (mensaje)
 
 def  procedimiento_suma (valor):
   valor = int (valor[0]) + int(valor[1]) + int(valor[2])
   return valor
 mostrar_mensaje = lambda mensaje : print(mensaje)#lamda para fragmentar el codigo el cual nos da un show message
+generar_mensaje = lambda mensaje : print(mensaje)
+ingrese_real = lambda mensaje : int(input(mensaje))
