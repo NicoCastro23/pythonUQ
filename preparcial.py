5f25d820377f1165f2aa2d0ef5d749bb03a67a19
Nico <jnch2005@gmail.com>
2023-04-27 17:00:35 -0500

    porfin

--- preparcial.py
+++ preparcial.py
@@ -1,19 +1,6 @@
-ingrese_real = lambda mensaje : int(input(mensaje))
-def calcular_serie(n):
-    resultado = 0
-    signo = 1
-    for i in range(1, n):
-        elemento = i * 9 - (i // 2) * 4
-        if n % 2 == 0:
-            signo = 1
-            resultado += signo * elemento
-            print(elemento)
-        else:
-            signo *= -1
-            resultado += signo * elemento
-    return resultado
-
-numero_uno = ingrese_real("Ingrese el límite inferior: ")
-do = calcular_serie(numero_uno)
-print(do)
-
+import functools
+give_integer = lambda mensaje : int (input(mensaje))#lamda para fragmentar el codigo el cual nos permite ingresar un integer
+lista = [0, 9, -4, 15, -8, 21, -12, 27, -16, 33, -20]
+numero_uno = give_integer("Ingrese el límite inferior: ")
+result = functools.reduce(lambda counter, item : counter + item, lista[0 : numero_uno])
+print(result)
