01659adccb70d55c305e871db7fd15d169946545
pola <santiagopolania88@gmail.com>
2023-05-03 10:37:45 -0500

    Primer trabajo arrays

---
+++ arrays_1.py
@@ -0,0 +1,34 @@
+mensaje = lambda mensaje, mensaje2, mensaje3, mensaje4 : print(mensaje, mensaje2, mensaje3, mensaje4)
+def definir():
+    cantidad = int(input("definir cantidad:"))
+    valores =[0.0]* cantidad
+    for i in range(cantidad):
+        valores[i] = float(input("ingrese un numero:"))
+    return valores
+def condition(nota):
+    condicion = True
+    if nota >= 3.0:
+        return condicion
+    else:
+        return not condicion
+def condition_2(n):
+    condicion = True
+    if n <= 3.0:
+        return condicion
+    else:
+        return not condicion
+def loop (n):
+    ganados = 0
+    perdidos = 0 
+    for notas in n:
+        if condition(notas):
+            ganados += 1
+        elif condition_2(notas):
+            perdidos +=1
+        mensaje("ganaron:", ganados,"perdieron:", perdidos)  
+
+def main():
+    nota = [1, 2, 3, 4, 5]
+    deini = definir()
+    lop = loop(deini)
+main()
