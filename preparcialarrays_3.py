a852e7547522d0d48b284f5f20501dc027326ff6
Nico <jnch2005@gmail.com>
2023-05-25 14:56:09 -0500

    tosavia no funciona

---
+++ preparcialarrays_3.py
@@ -0,0 +1,27 @@
+ingresar_entero = lambda mensaje :int(input(mensaje))
+def definir(cantidad):
+    valores =[""]* cantidad
+    for i in range(cantidad):
+        valores[i] = str(input("ingrese un nombre:"))
+    return valores
+
+def filtrar(lista):
+    lista_nueva = []
+    for i in lista:
+        if i.startswith("j"):
+            lista_nueva.append(i)
+    return lista_nueva
+
+def generar_mensaje(dato):
+    mensaje = f"los nombres que empiezan con j son: {dato}"
+    return generar_mensaje
+mostrar_mensaje = lambda mensaje :print(mensaje)
+
+def main():
+    ingresar_numero = ingresar_entero("ingresar numero de estudiantes:")
+    ingresar_lista = definir(ingresar_numero)
+    mensaje = generar_mensaje(ingresar_lista)
+    mostrar_mensaje(mensaje)
+main()
+
+
